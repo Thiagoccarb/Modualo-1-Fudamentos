4010ca253e4642c13414cfeeadc366990d3ebcc5
Thiago <thiagoc.carboneri@gmail.com>
2021-06-10 19:14:27 -0300

    form pronto com uso de biblioteca bootstrap

--- Bloco-6/Dia-2/script.js
+++ Bloco-6/Dia-2/script.js
@@ -1,1 +1,12 @@
-const states = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO', 'DF'];
+function createStates(){
+    const states = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO', 'DF'];
+    const select = document.getElementById('estados');
+    for (let i = 0; i < states.length; i++) {
+        let option = document.createElement('option');
+        const element = states[i];
+        option.value = element;
+        option.innerText = element;
+        select.appendChild(option);
+    }
+}
+createStates();
