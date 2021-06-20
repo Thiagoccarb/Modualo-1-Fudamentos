d93f83a8425de904ac96adc64f598124b15dc1d4
Thiago <thiagoc.carboneri@gmail.com>
2021-06-20 19:13:30 -0300

    requisito 21

--- trybewarts/sd-013-a-project-trybewarts/script.js
+++ trybewarts/sd-013-a-project-trybewarts/script.js
@@ -1,3 +1,13 @@
+const agreementInput = document.getElementById('agreement');
+const inputSubmitButton = document.getElementById('submit-btn');
+const inputCounter = document.getElementById('counter');
+const textarea = document.getElementById('textarea');
+const inputs = document.getElementsByTagName('input');
+const div = document.querySelector('.data-container');
+const form = document.getElementById('evaluation-form');
+const selected = document.querySelector('#house').selectedOptions[0];
+const radios = document.querySelectorAll('input[name="family"]');
+
 function checkData() {
   const inputButton = document.querySelector('.loginButton');
   const loginInput = document.getElementById('login');
@@ -15,8 +25,6 @@ function checkData() {
 checkData();
 
 function prevent() {
-  const agreementInput = document.getElementById('agreement');
-  const inputSubmitButton = document.getElementById('submit-btn');
   agreementInput.addEventListener('click', () => {
     if (agreementInput.checked) {
       inputSubmitButton.removeAttribute('disabled');
@@ -27,8 +35,6 @@ function prevent() {
 prevent();
 
 function counter() {
-  const inputCounter = document.getElementById('counter');
-  const textarea = document.getElementById('textarea');
   textarea.addEventListener('keyup', () => {
     if (textarea.value.length >= 0) {
       inputCounter.innerText = '500' - textarea.value.length;
@@ -37,3 +43,44 @@ function counter() {
 }
 
 counter();
+
+// // const hideForm = () => {
+// //   form.style.display = 'none';
+// // }
+
+// // inputSubmitButton.addEventListener('click', hideForm);
+
+// const inputData = () => {
+//     // inputs
+//     for (let index = 0; index < 5; index++) {
+//       const element = inputs[index];
+//       const p = document.createElement('p');
+//       p.innerText = `${element.name}: ${element.value}`;
+//       div.appendChild(p);
+//     }
+// }
+
+
+
+// const inputSelect = () => {
+//     const p = document.createElement('p');
+//     p.innerHTML = `Casa: ${selected.value}`;
+//     div.appendChild(p);
+// }
+
+
+// const inputRadio = () => {
+//     for (let i = 0; i < radios.length; i++) {
+//       const element = radios[i];
+//       if (element.checked) {
+//         p = document.createElement('p');
+//         p.innerHTML = element.checked.value;
+//         div.appendChild(p);
+//         break;
+//       }
+//     }
+// }
+
+
+
+
