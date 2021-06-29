8458938c7f9a787a93f35c65b4b80ea18439b021
Thiago <thiagoc.carboneri@gmail.com>
2021-06-28 22:40:17 -0300

    exercicios práticos

---
+++ Bloco-8/Dia-4/exercicios-praticos/exemplo_aula.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+
+const players = [
+  { fullName: 'Adriano Imperador', email: 'didico@futebol.br' },
+  { fullName: 'Ronaldinho Gaúcho', email: 'bruxo@futebol.br' },
+  { fullName: 'Ronaldo Fenômeno', email: 'cortedocascao@futebol.br' },
+  { fullName: 'Marta Vieira da Silva', email: 'rainhamarta@futebol.br' },
+];
+
+// crie um objeto com o nome de cada jogador e e-mail
+
+const newPlayers = players.reduce((accumulator, currentValue) => {
+  console.log('accumulator:', accumulator, 'currentValue:', currentValue);
+  accumulator[currentValue.fullName] = currentValue.email;
+  return accumulator;
+},{});
+
+console.log(newPlayers);
+
+//Testes
+const expectedValues = {
+  'Adriano Imperador': 'didico@futebol.br',
+  'Ronaldinho Gaúcho': 'bruxo@futebol.br',
+  'Ronaldo Fenômeno': 'cortedocascao@futebol.br',
+  'Marta Vieira da Silva': 'rainhamarta@futebol.br'
+};
+
+//assert.deepStrictEqual(newPlayers, expectedValues); 
